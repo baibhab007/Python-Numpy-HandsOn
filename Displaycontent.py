3270247bd661c0bea708bbdcd8926eb3c01f659c
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-08-12 23:00:25 +0200

    Create Displaycontent.py

---
+++ Displaycontent.py
@@ -0,0 +1,13 @@
+####
+Try it Out - Display Content
+Open a IPython Console and perform the following tasks.
+
+Define the list fruits = ['apple', 'mango', 'kiwi', 'watermelon', 'pear'] in first cell.
+
+In second cell type fru and press tab.
+
+Simply display content of list fruits.
+####
+
+fruits = ['apple', 'mango', 'kiwi', 'watermelon', 'pear']
+print(fruits)
