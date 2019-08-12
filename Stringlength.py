bec102b71c64ff488d2de14f3587f7234db18a7d
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-08-12 23:16:03 +0200

    Create Stringlength.py

---
+++ Stringlength.py
@@ -0,0 +1,12 @@
+####
+Try it Out - String Length
+Determine the length of each defined fruit names and save it in list fruits_len.
+
+Hint: Use list Comprehension
+Display contents of fruits_len .
+
+Find the fruit names that start with 'm' or 'p' and save it in list fruits_mp.
+
+Hint: Use list Comprehension
+Display contents of fruits_mp.
+####
