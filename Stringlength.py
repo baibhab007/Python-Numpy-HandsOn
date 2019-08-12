b23efce69847e7e92e35f542976f49b3408c0b56
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-08-12 23:18:20 +0200

    Update Stringlength.py

--- Stringlength.py
+++ Stringlength.py
@@ -10,3 +10,14 @@ Find the fruit names that start with 'm' or 'p' and save it in list fruits_mp.
 Hint: Use list Comprehension
 Display contents of fruits_mp.
 ####
+
+
+fruits = ['apple', 'mango', 'kiwi', 'watermelon', 'pear']
+fruits_len = []
+fruits_mp = []
+for x in fruits:
+  fruits_len.append(len(x))
+  if (x.startswith('m') or x.startswith('p')):
+    fruits_mp.append(x)
+print(fruits_len)
+print(fruits_mp)
