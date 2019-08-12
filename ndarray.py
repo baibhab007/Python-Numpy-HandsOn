0660c8c9c6ce71ffe22a4f5d3d30abb7ffeede79
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-08-12 23:04:17 +0200

    Create ndarray.py

---
+++ ndarray.py
@@ -0,0 +1,21 @@
+####
+Try it Out - ndarray
+Import numpy package as np
+
+Define a ndarray x1 from list, `[[[-1,1],[-2,2]],[[-3, 3], [-4, 4]]]
+
+Determine the following attributes of x1
+
+Number of dimensions
+
+Shape
+
+Size
+####
+
+
+import numpy as np
+x1 = np.array([[[-1,1],[-2,2]],[[-3, 3], [-4, 4]]])
+print(x1.ndim)
+print(x1.shape)
+print(x1.size)
