d18755c6f011480358c9370b8e090d892cb2d624
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-08-12 23:46:18 +0200

    Create Moreonndarray.py

---
+++ Moreonndarray.py
@@ -0,0 +1,15 @@
+####
+Try it Out - More on ndarray
+Define a ndarray x6, having first 20 even numbers.
+
+Hint : Make use of arange.
+Define a ndarray x7, having equally spaced 30 numbers between 10 and 20.
+
+Hint : Make use of linspace.
+####
+
+import numpy as np
+x6 = np.arange(1,21)
+x7 = np.linspace(10,20,30)
+print(x6)
+print(x7)
