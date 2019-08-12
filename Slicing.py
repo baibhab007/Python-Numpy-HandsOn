a8315bb6cbc257ea20d12f1461e3639155aafb2a
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-08-12 23:24:09 +0200

    Create Slicing.py

---
+++ Slicing.py
@@ -0,0 +1,12 @@
+####
+Try it Out - Slicing
+Create a array x of shape (2, 3, 5), having first 30 natural numbers.
+
+Create a boolean array b of shape (2,), having elements True, False.
+
+Try the following expressions
+
+x[b]
+
+x[b,:,1:3]
+####
