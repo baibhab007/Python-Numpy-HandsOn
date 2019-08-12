7fe890f44befc6e6ad0a9ab487b6a36b058cb6c4
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-08-12 22:41:53 +0200

    Create Displaylistitems.py

---
+++ Displaylistitems.py
@@ -0,0 +1,23 @@
+####
+Try it Out - Display List Items
+Determine the length of each defined fruit names and save it in list fruits_len.
+
+Hint: Use list Comprehension
+Display contents of fruits_len
+
+Find the fruit names that start with 'm' or 'p' and save it in list fruits_mp
+
+Hint: Use list Comprehension
+Display contents of fruits_mp
+####
+
+
+fruits = ['apple', 'mango', 'kiwi', 'watermelon', 'pear']
+fruits_len = []
+fruits_mp = []
+for x in fruits:
+  fruits_len.append(len(x))
+  if (x.startswith('m') or x.startswith('p')):
+    fruits_mp.append(x)
+print(fruits_len)
+print(fruits_mp)
